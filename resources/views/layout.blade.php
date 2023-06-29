8be75e78b6e7b087b468ef25ca447061af53d55a
CrGarroHernandez <c.garro.hernandez@gmail.com>
2023-06-29 15:38:01 -0400

    favicon commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="{{asset('img/buon.png')}}">
         <title>@yield('title', 'Buon Café')</title>
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
